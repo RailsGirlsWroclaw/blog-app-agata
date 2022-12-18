c01d0c0481bb4119d9a7c515d7a287d3c11045c3
Agata Mostowik <98911083+agatamostowik@users.noreply.github.com>
2022-12-18 12:48:20 +0000

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 end
+
+Rails.application.routes.draw do
+  # root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+end
